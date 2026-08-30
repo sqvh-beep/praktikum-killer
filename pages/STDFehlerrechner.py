00db09b838939c8f6f653c5d43a3e29672b7736a
Philipp <sqvh13673@gmail.com>
2026-08-30 21:08:47 +0200

    Update STDFehlerrechner.py

--- pages/STDFehlerrechner.py
+++ pages/STDFehlerrechner.py
@@ -37,7 +37,7 @@ if file:
 
     # 3. Einlesen in Pandas (on_bad_lines='skip' verhindert Abstürze bei kaputten Reihen)
     try:
-        df = pd.read_csv(io.StringIO(text_data), sep=sep, on_bad_lines='skip')
+        df = pd.read_csv(io.StringIO(text_data), sep=sep, on_bad_lines='skip', decimal=',')
     except pd.errors.EmptyDataError:
         st.error("Die hochgeladene Datei ist leer.")
         st.stop()
